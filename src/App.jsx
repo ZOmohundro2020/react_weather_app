1da965265243ffcb4f8d34e12b3e8c4129c14813
ZOmohundro <zaneomohundro@gmail.com>
2023-11-20 21:26:07 -0500

    Move fetching logic down prop tree to the component that needs the data

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import "./App.css";
 import Header from "./components/Header.jsx";
 import CityInput from "./components/CityInput.jsx";
@@ -9,8 +9,6 @@ function App() {
     { name: "Initial Test City", current: 40, low: 36, high: 45 },
   ]);
 
-  const [currentConditions, setCurrentConditions] = useState({});
-
   function updateCities(inputFieldData) {
     setCityList([
       { name: inputFieldData, current: "00", low: "00", high: "00" },
@@ -18,38 +16,14 @@ function App() {
     ]); //remember these are strings
   }
 
-  useEffect(() => {
-    // declare the data fetching function
-    const fetchData = async () => {
-      const response = await fetch(
-        "https://api.open-meteo.com/v1/forecast?latitude=45.4112&longitude=-75.6981&current=temperature_2m&daily=temperature_2m_max,temperature_2m_min&temperature_unit=fahrenheit&timezone=America%2FNew_York&forecast_days=1"
-      );
-      const data = await response.json();
-      // console.log(data);
-      // console.log(data.current.temperature_2m);
-      // console.log(data.daily.temperature_2m_min);
-      // console.log(data.daily.temperature_2m_max);
-
-      setCurrentConditions({ ...data });
-    };
-
-    // call the function
-    fetchData()
-      // make sure to catch any error
-      .catch(console.error);
-
-    console.log("current conditions is:");
-    console.log(currentConditions);
-  }, []);
-
   return (
     <>
-      {import.meta.env.VITE_SECRET_KEY}
-      {import.meta.env.MY_KEY}
+      {/* {import.meta.env.VITE_SECRET_KEY}
+      {import.meta.env.MY_KEY} */}
 
       <Header />
       <CityInput addCity={updateCities} />
-      <CityWeather cityList={cityList} fetchedWeather={currentConditions} />
+      <CityWeather cityList={cityList} />
     </>
   );
 }
--- src/components/CityWeather.jsx
+++ src/components/CityWeather.jsx
@@ -1,6 +1,49 @@
-export default function CityWeather({ cityList, fetchedWeather }) {
-  console.log("cityList prop is:");
-  console.log(cityList);
+import { useState, useEffect } from "react";
+
+export default function CityWeather({ cityList }) {
+  console.log("cityList prop is:", cityList);
+
+  const [currentConditions, setCurrentConditions] = useState({});
+  const [isFetching, setisFetching] = useState(false);
+  const [error, setError] = useState();
+
+  useEffect(() => {
+    // declare the data fetching function
+    const fetchData = async () => {
+      setisFetching(true);
+
+      try {
+        const response = await fetch(
+          "https://api.open-meteo.com/v1/forecast?latitude=45.4112&longitude=-75.6981&current=temperature_2m&daily=temperature_2m_max,temperature_2m_min&temperature_unit=fahrenheit&timezone=America%2FNew_York&forecast_days=1"
+        );
+        const data = await response.json();
+
+        if (!response.ok) {
+          throw new Error("failed to load data");
+        }
+
+        setCurrentConditions({ ...data });
+      } catch (error) {
+        setError(error.message);
+      }
+
+      // console.log(data);
+      // console.log(data.current.temperature_2m);
+      // console.log(data.daily.temperature_2m_min);
+      // console.log(data.daily.temperature_2m_max);
+
+      setisFetching(false);
+    };
+
+    // call the function
+    fetchData();
+  }, []);
+
+  if (error) {
+    console.log(error);
+  }
+
+  console.log("current conditions is:", currentConditions);
 
   const citiesList = cityList.map((city, index) => (
     <div key={index}>
@@ -17,5 +60,5 @@ export default function CityWeather({ cityList, fetchedWeather }) {
     </div>
   ));
 
-  return <>{citiesList}</>;
+  return <article>{isFetching ? <p>Loading</p> : citiesList}</article>;
 }
